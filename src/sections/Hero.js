fbf097914b9abaea72099265006148b6bde6ff82
Debora Cibu <d.cibu@yahoo.com>
2022-08-17 17:08:04 +0300

    changed hero to full width

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -4,8 +4,8 @@ import hero from "../static/hero.png";
 
 const Hero = () => {
   return (
-    <section className="container w-11/12 max-w-screen-xl mx-auto relative">
-        <img src={hero} alt="Ora Lui Robert" />
+    <section className=" relative">
+        <img src={hero} className="w-full" alt="Ora Lui Robert" />
     </section>
   );
 };
