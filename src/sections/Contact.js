ed745c977b9ad0c67a14bda5f9459fb88a2534a9
Debora Cibu <d.cibu@yahoo.com>
2022-08-25 15:52:26 +0300

    Added contact section

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,14 +1,39 @@
 import React from "react";
+import { FaFacebookSquare, FaTwitterSquare, FaInstagramSquare } from "react-icons/fa";
 
 const Contact = () => {
   return (
-    <section className="container w-11/12 max-w-screen-lg mx-auto py-20 md:pt-40 lg:pt-60">
-      <div className="max-w-sm">
-        <h3 className="text-2xl font-semibold mb-5 md:text-4xl">Get in touch</h3>
-        <p className="text-lg text-gray-400 mb-10 leading-8 md:leading-10 md:text-2xl">We’re located in Timisoara, Romania and help startups around the world.</p>
-        <a href="mailto:contact@qed.team" target="_self" className="text-blue-50 text-lg leading-10 border-b-2 border-blue-50 font-semibold inline-block md:text-2xl">
-          contact@qed.team
-        </a>
+    <section className="container mx-auto flex py-20">
+      <div className="w-1/2">
+        <h3 className="text-2xl font-semibold mb-5 md:text-4xl">Contact</h3>
+        <div className="ourEmail mb-6">
+          <b className="block">Email:</b>
+          <a href="mailto:oraluirobert.com">contact@oraluirobert.com</a>
+        </div>
+        <div className="phone mb-7">
+          <b className="">Phone:</b>
+          <p>+40123456789</p>
+        </div>
+        <div className="social-media flex">
+          <a className="mr-2 text-2xl" href="www.facebook.com/oraluirobert" target="_blank">
+            <FaFacebookSquare />
+          </a>
+          <a className="mr-2 text-2xl" href="" target="_blank">
+            <FaTwitterSquare />
+          </a>
+          <a className="text-2xl" href="www.instagram.com/oraluirobert" target="_blank">
+            <FaInstagramSquare />
+          </a>
+        </div>
+      </div>
+      <div className="w-1/2">
+        <h3 className="text-2xl font-semibold mb-5 md:text-4xl">Get more information</h3>
+        <div className="flex flex-col items-start max-w-md">
+          <input className="name border-b-2 pb-1 w-full" placeholder="Name" />
+          <input className="email border-b-2 pb-1 my-5 w-full" placeholder="Email" />
+          <textarea placeholder="Enter your message" className="border-2 w-full " rows="3"></textarea>
+          <button className="bg-blue-100 py-2 px-10 mt-5 text-white ">Send Message</button>
+        </div>
       </div>
     </section>
   );
