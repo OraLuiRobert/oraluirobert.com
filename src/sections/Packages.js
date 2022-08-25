733bb930297b7664bb90ee5976f0e94ff0751385
Debora Cibu <d.cibu@yahoo.com>
2022-08-25 15:44:30 +0300

    Added packages section

---
+++ src/sections/Packages.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "../ui/molecules/Card";
+import SectionTitle from "../ui/molecules/SectionTitle";
+
+import sponsorPackages from "../data/packages.json";
+
+const Packages = () => {
+  return (
+    <div id="packages" className="py-10">
+      <SectionTitle title="Sponsor Packages" />
+      <div className="container  mx-auto flex justify-around items-stretch ">
+        {sponsorPackages.packages.map((item, index) => (
+          <Card packageName={item.name} packagePrice={item.price} packageBenefits={item.benefits} index={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Packages;
---
+++ src/ui/molecules/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaCheck } from "react-icons/fa";
+
+const Card = ({ packageName, packagePrice, packageBenefits, index }) => {
+  const gradientSetings = [
+    { gradientFrom: "from-yellow-50", gradientVia: "via-yellow-500", gradientTo: "to-red-400" },
+    { gradientFrom: "from-red-400", gradientVia: "via-rose-600", gradientTo: "to-pink-900" },
+    { gradientFrom: "from-pink-900", gradientVia: "via-fuchsia-900", gradientTo: "to-indigo-900" },
+  ];
+  return (
+    <div
+      className={`flex flex-col justify-between p-10 rounded-2xl flex-1 mx-5 text-center text-white shadow-2xl bg-gradient-to-br  ${gradientSetings[index].gradientFrom} ${gradientSetings[index].gradientVia} ${gradientSetings[index].gradientTo}`}
+    >
+      <div className="description">
+        <h1 className="font-bold text-2xl rounded-full">{packageName}</h1>
+        <h2 className="py-5 rounded-full ">{packagePrice}</h2>
+        {packageBenefits.map((benefit, index) => (
+          <div className="flex items-start" key={index}>
+            <FaCheck className="mt-1" />
+            <p className="text-left ml-2">{benefit}</p>
+          </div>
+        ))}
+      </div>
+      <div className="cardFooter">
+        <button className="bg-white text-black px-6 py-2 rounded-xl font-bold mt-5 ">Order Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
