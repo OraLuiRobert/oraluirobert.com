bb86d971c0446e5b55a6592ed89e7f0a3a8b14bc
Debora Cibu <d.cibu@yahoo.com>
2022-08-29 22:06:24 +0300

    new section added with timeline plugin

---
+++ src/data/timeline.js
@@ -0,0 +1,19 @@
+const timeline = [
+  {
+    date: "2018-09-01",
+    location: "Vrancea",
+    description: "In three educational institutions from Vrancea: 120 middle school and high school students participated.",
+  },
+  {
+    date: "2021-09-01",
+    location: "Timisoara",
+    description: "In 2021 we worked with 100 children, that live in foster care and other state institutions.",
+  },
+  {
+    date: "2022-09-01",
+    location: "Timisoara",
+    description: "In 2022 we worked with 70 children, that live in foster care and other state institutions, in a big project.",
+  },
+];
+
+export default timeline;
---
+++ src/sections/Projects.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import HorizontalTimeline from "react-horizontal-timeline";
+import SectionTitle from "../ui/molecules/SectionTitle";
+import timeline from "../data/timeLine";
+
+const Projects = () => {
+  const [values, setValues] = useState({ value: 0, previous: -1 });
+
+  return (
+    <section id="projects" className="py-20">
+      <SectionTitle title="Past Projects" />
+      <div
+        className=" w-6/12 mx-auto"
+        style={{
+          height: "100px",
+          fontSize: "15px",
+        }}
+      >
+        <HorizontalTimeline
+          styles={{
+            background: "#ffffff",
+            foreground: "#6EA6C3",
+            outline: "#dfdfdf",
+          }}
+          labelWidth={100}
+          linePadding={280}
+          minEventPadding={60}
+          maxEventPadding={60}
+          index={values.value}
+          indexClick={(index) => {
+            setValues({ value: index, previous: values.value });
+          }}
+          values={timeline.map((x) => x.date)}
+        />
+      </div>
+      <div className="container mx-auto w-5/12 text-left p-10 rounded-md shadow-2xl bg-gray-200">
+        <h1 className="font-bold pb-5 text-xl">Location: {timeline[values.value].location}</h1>
+        <p className="min-h-[50px]">{timeline[values.value].description}</p>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
