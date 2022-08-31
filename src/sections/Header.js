0f9058e2f6d0e8d6e74bf00936722bac7c9b91f4
Debora Cibu <d.cibu@yahoo.com>
2022-08-31 08:05:28 +0300

    added width to the header

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -9,11 +9,13 @@ const Header = () => {
   const background = position > 50 ? "bg-blue-50" : "bg-blue-100";
 
   return (
-    <section id="header" className={"fixed top-0 right-0 left-0 z-10 py-3 px-5 flex justify-between items-center " + background}>
-      <a href="/" target="_self" className="block mb-3 sm:mb-0">
-        <img src={logo} className="w-[80px]" alt="Ora Lui Robert" />
-      </a>
-      <Navigation />
+    <section id="header" className={" fixed top-0 right-0 left-0 z-10 py-3 px-5 " + background}>
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="/" target="_self" className="block mb-3 sm:mb-0">
+          <img src={logo} className="w-[80px]" alt="Ora Lui Robert" />
+        </a>
+        <Navigation />
+      </div>
     </section>
   );
 };
