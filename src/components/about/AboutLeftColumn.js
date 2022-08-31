6c1dc46064d5e3df613a086908b894bc32529e3e
Debora Cibu <d.cibu@yahoo.com>
2022-08-31 09:28:46 +0300

    align text with images

--- src/components/about/AboutLeftColumn.js
+++ src/components/about/AboutLeftColumn.js
@@ -3,19 +3,19 @@ import React from "react";
 const AboutLeftColumn = ({ title, description, secondTitle, secondDescription, image }) => {
   return (
     <>
-      <div className="flex-1 px-10 text-lg text-center">
+      <div className="flex-1 text-lg text-center">
         <div className="mb-10">
           <h1 className="text-2xl py-2 font-bold">{title}</h1>
-          <p>{description}</p>
+          <p className="text-left">{description}</p>
         </div>
         {secondTitle && (
           <div>
             <h1 className="text-2xl py-2 font-bold">{secondTitle}</h1>
-            <p>{secondDescription}</p>
+            <p className="text-left">{secondDescription}</p>
           </div>
         )}
       </div>
-      <div className="flex-1">
+      <div className="flex-1 ml-20">
         <img src={image} alt="Robert's Hour" />
       </div>
     </>
