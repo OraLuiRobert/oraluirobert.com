ce655c8fbe894d025b8a49cfa5b381eee000b941
Debora Cibu <d.cibu@yahoo.com>
2022-08-31 09:32:48 +0300

    fixed the contanct section

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,4 +1,7 @@
 import React from "react";
+
+import phoneNumber from "../static/phone-number.png";
+
 import { FaFacebookSquare, FaTwitterSquare, FaInstagramSquare } from "react-icons/fa";
 
 const Contact = () => {
@@ -13,16 +16,16 @@ const Contact = () => {
           </div>
           <div className="phone mb-7">
             <b className="">Phone:</b>
-            <p>+40123456789</p>
+            <img src={phoneNumber} />
           </div>
           <div className="social-media flex">
-            <a className="mr-2 text-2xl" href="www.facebook.com/oraluirobert" target="_blank">
+            <a className="mr-2 text-2xl" href="https://www.facebook.com/OraLuiRobert/" target="_blank">
               <FaFacebookSquare />
             </a>
-            <a className="mr-2 text-2xl" href="" target="_blank">
+            <a className="mr-2 text-2xl" href="https://www.twitter.com/OraLuiRobert/" target="_blank">
               <FaTwitterSquare />
             </a>
-            <a className="text-2xl" href="www.instagram.com/oraluirobert" target="_blank">
+            <a className="text-2xl" href="https://www.instagram.com/oraluirobert/" target="_blank">
               <FaInstagramSquare />
             </a>
           </div>
@@ -30,9 +33,9 @@ const Contact = () => {
         <div className="w-1/2">
           <h3 className="text-2xl font-semibold mb-5 md:text-4xl">Get more information</h3>
           <div className="flex flex-col items-start max-w-md">
-            <input className="name border-b-2 pb-1 pl-1 w-full" placeholder="Name" />
-            <input className="email border-b-2 pb-1 pl-1 my-5 w-full" placeholder="Email" />
-            <textarea placeholder="Enter your message" className="border-2 w-full pl-1" rows="3"></textarea>
+            <input className="name border-b-2 p-2 pl-2 w-full" placeholder="Name" />
+            <input className="email border-b-2 p-2 pl-2 my-5 w-full" placeholder="Email" />
+            <textarea placeholder="Enter your message" className="border-2 w-full pl-2" rows="3"></textarea>
             <button className="bg-blue-100 py-2 px-10 mt-5 text-white ">Send Message</button>
           </div>
         </div>
