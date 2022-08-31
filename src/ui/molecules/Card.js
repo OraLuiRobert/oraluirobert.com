88ba3fb84e8b965a0f9f0a26b97be420626ef3e7
Debora Cibu <d.cibu@yahoo.com>
2022-08-31 08:04:19 +0300

    removed margin

--- src/ui/molecules/Card.js
+++ src/ui/molecules/Card.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FaCheck } from "react-icons/fa";
 
 const Card = ({ packageName, packagePrice, packageBenefits, index }) => {
+  const hasMargin = index === 1 ? " mx-10" : "";
   const gradientSetings = [
     { gradientFrom: "from-yellow-50", gradientVia: "via-yellow-500", gradientTo: "to-red-400" },
     { gradientFrom: "from-red-400", gradientVia: "via-rose-600", gradientTo: "to-pink-900" },
@@ -9,7 +10,7 @@ const Card = ({ packageName, packagePrice, packageBenefits, index }) => {
   ];
   return (
     <div
-      className={`flex flex-col justify-between p-10 rounded-2xl flex-1 mx-5 text-center text-white shadow-2xl bg-gradient-to-br  ${gradientSetings[index].gradientFrom} ${gradientSetings[index].gradientVia} ${gradientSetings[index].gradientTo}`}
+      className={`flex flex-col justify-between p-10 rounded-2xl flex-1 text-center text-white shadow-2xl bg-gradient-to-br ${hasMargin} ${gradientSetings[index].gradientFrom} ${gradientSetings[index].gradientVia} ${gradientSetings[index].gradientTo}`}
     >
       <div className="description">
         <h1 className="font-bold text-2xl rounded-full">{packageName}</h1>
