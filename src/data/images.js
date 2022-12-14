5b4503f5bf9b1591c1b579373e938e2c8a71ef26
Debora Cibu <d.cibu@yahoo.com>
2022-12-14 12:38:11 +0100

    chore: update gallery
    replace gallery package with a new one

--- src/data/images.js
+++ src/data/images.js
@@ -10,13 +10,39 @@ import gallery9 from "../static/gallery/gallery-9.jpg";
 import gallery10 from "../static/gallery/gallery-10.jpg";
 import gallery11 from "../static/gallery/gallery-11.jpg";
 import gallery12 from "../static/gallery/gallery-12.jpg";
+import gallery13 from "../static/gallery/gallery-13.jpg";
+import gallery14 from "../static/gallery/gallery-14.jpg";
+import gallery15 from "../static/gallery/gallery-15.jpg";
+import gallery16 from "../static/gallery/gallery-16.jpg";
+import gallery17 from "../static/gallery/gallery-17.jpg";
+import gallery18 from "../static/gallery/gallery-18.jpg";
+import gallery19 from "../static/gallery/gallery-19.jpg";
+import gallery20 from "../static/gallery/gallery-20.jpg";
+import gallery21 from "../static/gallery/gallery-21.jpg";
+import gallery22 from "../static/gallery/gallery-22.jpg";
+import gallery23 from "../static/gallery/gallery-23.jpg";
+import gallery24 from "../static/gallery/gallery-24.jpg";
+import gallery25 from "../static/gallery/gallery-25.jpg";
+import gallery26 from "../static/gallery/gallery-26.jpg";
+import gallery27 from "../static/gallery/gallery-27.jpg";
+import gallery28 from "../static/gallery/gallery-28.jpg";
+import gallery29 from "../static/gallery/gallery-29.jpg";
+import gallery30 from "../static/gallery/gallery-30.jpg";
+import gallery31 from "../static/gallery/gallery-31.jpg";
+import gallery32 from "../static/gallery/gallery-32.jpg";
+import gallery33 from "../static/gallery/gallery-33.jpg";
+import gallery34 from "../static/gallery/gallery-34.jpg";
+import gallery35 from "../static/gallery/gallery-35.jpg";
+import gallery36 from "../static/gallery/gallery-36.jpg";
+import gallery37 from "../static/gallery/gallery-37.jpg";
+import gallery38 from "../static/gallery/gallery-38.jpg";
+import gallery39 from "../static/gallery/gallery-39.jpg";
+import gallery40 from "../static/gallery/gallery-40.jpg";
+import gallery41 from "../static/gallery/gallery-41.jpg";
+import gallery42 from "../static/gallery/gallery-42.jpg";
+import gallery43 from "../static/gallery/gallery-43.jpg";
 
 const galleryImages = [
-  {
-    src: gallery7,
-    thumbnail: gallery7,
-    desc: "Robert's Hour #7",
-  },
   {
     src: gallery1,
     thumbnail: gallery1,
@@ -47,7 +73,11 @@ const galleryImages = [
     thumbnail: gallery6,
     desc: "Robert's Hour #6",
   },
-
+  {
+    src: gallery7,
+    thumbnail: gallery7,
+    desc: "Robert's Hour #7",
+  },
   {
     src: gallery8,
     thumbnail: gallery8,
@@ -72,6 +102,161 @@ const galleryImages = [
     src: gallery12,
     thumbnail: gallery12,
     desc: "Robert's Hour #12",
+  },
+  {
+    src: gallery13,
+    thumbnail: gallery13,
+    desc: "Robert's Hour #13",
+  },
+  {
+    src: gallery14,
+    thumbnail: gallery14,
+    desc: "Robert's Hour #14",
+  },
+  {
+    src: gallery15,
+    thumbnail: gallery15,
+    desc: "Robert's Hour #15",
+  },
+  {
+    src: gallery16,
+    thumbnail: gallery16,
+    desc: "Robert's Hour #16",
+  },
+  {
+    src: gallery17,
+    thumbnail: gallery17,
+    desc: "Robert's Hour #17",
+  },
+  {
+    src: gallery18,
+    thumbnail: gallery18,
+    desc: "Robert's Hour #18",
+  },
+  {
+    src: gallery19,
+    thumbnail: gallery19,
+    desc: "Robert's Hour #19",
+  },
+  {
+    src: gallery20,
+    thumbnail: gallery20,
+    desc: "Robert's Hour #20",
+  },
+  {
+    src: gallery21,
+    thumbnail: gallery21,
+    desc: "Robert's Hour #21",
+  },
+  {
+    src: gallery22,
+    thumbnail: gallery22,
+    desc: "Robert's Hour #22",
+  },
+  {
+    src: gallery23,
+    thumbnail: gallery23,
+    desc: "Robert's Hour #23",
+  },
+  {
+    src: gallery24,
+    thumbnail: gallery24,
+    desc: "Robert's Hour #24",
+  },
+  {
+    src: gallery25,
+    thumbnail: gallery25,
+    desc: "Robert's Hour #25",
+  },
+  {
+    src: gallery26,
+    thumbnail: gallery26,
+    desc: "Robert's Hour #26",
+  },
+  {
+    src: gallery27,
+    thumbnail: gallery27,
+    desc: "Robert's Hour #27",
+  },
+  {
+    src: gallery28,
+    thumbnail: gallery28,
+    desc: "Robert's Hour #28",
+  },
+  {
+    src: gallery29,
+    thumbnail: gallery29,
+    desc: "Robert's Hour #29",
+  },
+  {
+    src: gallery30,
+    thumbnail: gallery30,
+    desc: "Robert's Hour #30",
+  },
+  {
+    src: gallery31,
+    thumbnail: gallery31,
+    desc: "Robert's Hour #31",
+  },
+  {
+    src: gallery32,
+    thumbnail: gallery32,
+    desc: "Robert's Hour #32",
+  },
+  {
+    src: gallery33,
+    thumbnail: gallery33,
+    desc: "Robert's Hour #33",
+  },
+  {
+    src: gallery34,
+    thumbnail: gallery34,
+    desc: "Robert's Hour #34",
+  },
+  {
+    src: gallery35,
+    thumbnail: gallery35,
+    desc: "Robert's Hour #35",
+  },
+  {
+    src: gallery36,
+    thumbnail: gallery36,
+    desc: "Robert's Hour #36",
+  },
+  {
+    src: gallery37,
+    thumbnail: gallery37,
+    desc: "Robert's Hour #37",
+  },
+  {
+    src: gallery38,
+    thumbnail: gallery38,
+    desc: "Robert's Hour #38",
+  },
+  {
+    src: gallery39,
+    thumbnail: gallery39,
+    desc: "Robert's Hour #39",
+  },
+  {
+    src: gallery40,
+    thumbnail: gallery40,
+    desc: "Robert's Hour #40",
+  },
+  {
+    src: gallery41,
+    thumbnail: gallery41,
+    desc: "Robert's Hour #41",
+  },
+  {
+    src: gallery42,
+    thumbnail: gallery42,
+    desc: "Robert's Hour #42",
+  },
+  {
+    src: gallery43,
+    thumbnail: gallery43,
+    desc: "Robert's Hour #43",
   },
 ];
 
